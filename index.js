22dc0a1605142f4b07735488d41bf4b56428838f
Nychtgian <nichtgian@gmail.com>
2018-01-24 11:07:13 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/script.js', function(req, res) {
+    res.sendFile(__dirname + '/public/script.js');
+});
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + '/public/style.css');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('art', function(save) {
+        io.emit('art', save);
+    });
+    socket.on('text', function(text) {
+        console.log('user disconnected222');
+        io.emit('text', text);
+    });
+});
+
+http.listen('3000', function() {
+    console.log('listening on 3000');
+});
+
+/*
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('art', function(save) {
+        io.emit('art', save);
+    });
+    socket.on('message', function(text) {
+        io.emit('message', text);
+    });
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});*/
---
+++ public/script.js
@@ -0,0 +1,107 @@
+let canvas = document.getElementById("art");
+let ctx = canvas.getContext("2d");
+let press = false;
+let lastx, lasty;
+
+window.addEventListener("resize", canvasSize, false);
+canvasSize();
+menuHide();
+chatHide();
+
+canvas.onmousedown =  function (e) {
+    if (document.getElementById("write").checked == true) {
+        ctx.font = document.getElementById("numb").value + "px Arial";
+        ctx.fillStyle = document.getElementById("color").value;
+        ctx.fillText(document.getElementById("text").value, e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+        return;
+    }
+    press = true;
+    draw(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, false);
+};
+
+canvas.onmousemove = function (e) {
+    sendArt();
+    if (press) {
+        draw(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, true);
+    }
+};
+
+canvas.onmouseup = function (e) {
+    press = false;
+    sendArt();
+};
+canvas.onmouseleave = function (e) {
+    press = false;
+    sendArt();
+};
+
+function sendArt() {
+    socket.emit('art',  canvas.toDataURL());
+}
+
+function sendText() {
+    socket.emit('text',  document.getElementById("message").value);
+    document.getElementById("message").value = "";
+}
+
+socket.on('art', function(save) {
+    var img = document.createElement("img");
+    img.src = save;
+    ctx.drawImage(img, 0, 0);
+});
+
+socket.on('text', function(text) {
+    var ul = document.getElementById("messages");
+    var li = document.createElement("li");
+    li.setAttribute("class", "text");
+    li.appendChild(document.createTextNode(text));
+    ul.appendChild(li);
+});
+
+function draw(x, y, down) {
+    if (down) {
+        ctx.beginPath();
+        ctx.strokeStyle = document.getElementById("color").value;
+        ctx.lineWidth = document.getElementById("numb").value;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    sendArt();
+    lastX = x;
+    lastY = y;
+}
+
+function canvasSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function colorChange() {
+    document.getElementById("menuShow").style.color = document.getElementById("color").value;
+    document.getElementById("down").style.color = document.getElementById("color").value;
+}
+
+function menuShow() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("menuShow").style.display = "none";
+    document.getElementById("nav").style.width = "450px";
+}
+
+function menuHide() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menuShow").style.display = "block";
+    document.getElementById("nav").style.width = "1em";
+}
+
+function chatShow() {
+    document.getElementById("main").style.display = "block";
+    document.getElementById("chat").style.display = "none";
+}
+
+function chatHide() {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("chat").style.display = "block";
+}
